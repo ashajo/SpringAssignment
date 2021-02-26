2d0a0b5593b095c447bcefa5b8cead5189fd8fae
asha joseph <ashajo183@gmail.com>
2021-02-26 20:35:06 +0100

    StudentDaoImplClass

--- src/main/java/se/lexicon/dao/StudentDaoImpl.java
+++ src/main/java/se/lexicon/dao/StudentDaoImpl.java
@@ -1,31 +1,40 @@
 package se.lexicon.dao;
 
+import se.lexicon.dao.sequencer.StudentSequencer;
 import se.lexicon.model.Student;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDaoImpl  implements StudentDao{
+    private List<Student> students = new ArrayList<>();
     @Override
     public Student save(Student student) {
+        if (student.getStudentId() ==0){
+            student.setStudentId(StudentSequencer.nextStudentId());
+            students.add(student);
+        }
 
-        return null;
+        return student;
     }
 
     @Override
     public Student find(int id) {
-        return null;
+        if (id == 0) throw new IllegalArgumentException("Id shouldn't be zero");
+        return students.stream().filter(student -> student.getStudentId()==id).findFirst().orElse(null);
     }
 
     @Override
     public List<Student> findAll() {
-        return null;
+        if (students == null)
+            System.out.println("The student list is empty");
+        return students;
     }
 
     @Override
     public void delete(int id) {
+        students.removeIf(student -> student.getStudentId()==id);
 
     }
 
-    private List<Student> students = new ArrayList<>();
 }
