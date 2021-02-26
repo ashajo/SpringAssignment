1f8f146dbb9d36ad2b3f9a05f9a9bee1253d5bbd
asha joseph <ashajo183@gmail.com>
2021-02-26 09:11:11 +0100

    StudentClass

---
+++ src/main/java/se/lexicon/model/Student.java
@@ -0,0 +1,58 @@
+package se.lexicon.model;
+
+import java.util.Objects;
+
+public class Student {
+
+    private int studentId;
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student(int studentId, String name) {
+        this.studentId = studentId;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Student() {
+    }
+}
